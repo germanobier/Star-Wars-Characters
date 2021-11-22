1d907ce99b6db82a5eac0645e7291c739cbdf561
Germano Bier <germanobier@hotmail.com>
2021-11-22 01:42:51 -0300

    first commit

---
+++ src/js/mail-validation.js
@@ -0,0 +1,25 @@
+const input_mail = document.querySelector(".input-mail")
+const invalid_mail = document.querySelector(".invalid-mail")
+const mailbox_button = document.querySelector(".mailbox-button")
+
+function validarEmail() {
+  if (input_mail.value.length >= 3 && input_mail.value.indexOf("@") != -1) {
+    validation = true
+      mailbox_button.classList.add("valid")
+  } else {
+    validation = false
+    mailbox_button.classList.remove("valid")
+  }
+
+  if (validation == false && input_mail.value.length > 0) {
+    input_mail.style.border = "1px solid #C47768"
+    invalid_mail.classList.add("on")
+  } else {
+    input_mail.style.border = ""
+    invalid_mail.classList.remove("on")
+  }
+}
+
+input_mail.addEventListener("keyup", () => {
+  validarEmail()
+})
