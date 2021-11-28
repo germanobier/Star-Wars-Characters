41faa9f2fb3334354bc0239953c23e1dbbe57dd7
Germano Bier <germanobier@hotmail.com>
2021-11-28 02:19:21 -0300

    result page sketch

--- src/js/mail-validation.js
+++ src/js/mail-validation.js
@@ -3,9 +3,11 @@ const invalid_mail = document.querySelector(".invalid-mail")
 const mailbox_button = document.querySelector(".mailbox-button")
 
 function validarEmail() {
+  let validation = null
+
   if (input_mail.value.length >= 3 && input_mail.value.indexOf("@") != -1) {
     validation = true
-      mailbox_button.classList.add("valid")
+    mailbox_button.classList.add("valid")
   } else {
     validation = false
     mailbox_button.classList.remove("valid")
